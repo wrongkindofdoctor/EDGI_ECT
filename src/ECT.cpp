45f58299e66161719af69d4e8b9fb4e9703cd836
Jessica Liptak <drjessliptak@gmail.com>
2019-01-08 17:16:25 -0500

    created a directory for source code andthe main ECT driver file

---
+++ src/ECT.cpp
@@ -0,0 +1,8 @@
+// This is the main program launched by the run script.
+/* It reads data from a list of user-specified netCDF files, runs QC and dimension re-ordering procedures on the data, and applies data reduction functions from the EDGI library to the data. Te user specifies whether the results are written to a summary file, or are compared to the statistics in a pre-existing summary file
+ */
+int main ()
+{
+	// Read in data from netCDF files(s)
+
+}
